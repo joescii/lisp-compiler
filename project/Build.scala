6060d95efcde0c48518719bbedf534834c8baf82
Joe Barnes <barnesjd@gmail.com>
2014-11-05 22:59:58 -0600

    Defined all of the subprojects I currently imagine I want

--- project/Build.scala
+++ project/Build.scala
@@ -7,6 +7,7 @@ object CompilerBuild extends Build {
     organization := "com.joescii",
     scalaVersion := "2.10.4",
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature"),
+    version := "0.0.1",
     resolvers ++= Seq(
       "sonatype-public" at "https://oss.sonatype.org/content/groups/public"
     )
@@ -17,7 +18,6 @@ object CompilerBuild extends Build {
     base = file("parser"),
     settings = commonSettings ++ Seq(
       name := "parser",
-      version := "0.0.1",
       description := "Source parser",
       libraryDependencies ++= Seq(
         "org.scalatest" % "scalatest_2.10" % "2.2.1" % "test"
@@ -25,9 +25,54 @@ object CompilerBuild extends Build {
     )
   )
 
+  lazy val jvm_target = Project(
+    id = "jvm-target",
+    base = file("jvm-target"),
+    settings = commonSettings ++ Seq(
+      name := "jvm-target",
+      description := "JVM bytecode target compiler",
+      libraryDependencies ++= Seq(
+      )
+    )
+  ).dependsOn(parser)
+
+  lazy val js_target = Project(
+    id = "js-target",
+    base = file("js-target"),
+    settings = commonSettings ++ Seq(
+      name := "js-target",
+      description := "JavaScript target compiler",
+      libraryDependencies ++= Seq(
+      )
+    )
+  ).dependsOn(parser)
+  
+  lazy val compiler = Project(
+    id = "compiler",
+    base = file("compiler"),
+    settings = commonSettings ++ Seq(
+      name := "compiler",
+      description := "All of this crap rolled into a compiler",
+      libraryDependencies ++= Seq(
+      )
+    )
+  ).dependsOn(parser, jvm_target, js_target)
+  
+  lazy val plugin = Project(
+    id = "plugin",
+    base = file("plugin"),
+    settings = commonSettings ++ Seq(
+      name := "plugin",
+      description := "The compiler packaged as an sbt plugin",
+      sbtPlugin := true,
+      libraryDependencies ++= Seq(
+      )
+    )
+  ).dependsOn(compiler)
+
   lazy val root = Project(
     id = "root",
     base = file(".")
-  ).aggregate(parser)
+  ).aggregate(parser, jvm_target, js_target, compiler, plugin)
 
 }
